2cc4dc8e35808d2cdd69f82bafd49a037472cd10
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2022-05-29 21:49:38 +0300

    feat(config):  - change base url for server

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth"
 import RedditProvider from "next-auth/providers/reddit"
 
 export default NextAuth({
+  site: process.env.NEXTAUTH_URL,
   providers: [
     RedditProvider({
       clientId: process.env.NEXT_PUBLIC_REDDIT_CLIENT_ID,
